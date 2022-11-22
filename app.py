1858bd6049b225903b81bd1d9b4d344a5b917f5f
HuangYiChen <s1094343@g.ncyu.edu.tw>
2022-11-22 23:21:02 +0800

    host=0.0.0.0

--- app.py
+++ app.py
@@ -121,4 +121,4 @@ def categories():
 		return jsonify ({"error":True,"message":"伺服器內部錯誤"}),500
 
 if __name__ == '__main__':
-	app.run(port=3000,debug=True)
+	app.run(host=0.0.0.0,port=3000,debug=True)
