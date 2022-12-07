8fb27df9c9279b0363b640ba4f57527e4721afd4
HuangYiChen <s1094343@g.ncyu.edu.tw>
2022-12-08 01:21:16 +0800

    week3 second version

--- data/db.py
+++ data/db.py
@@ -1,15 +1,29 @@
 import mysql.connector
 import json
+from mysql.connector import Error
+from mysql.connector import pooling
 
-db = mysql.connector.connect(
+connection_pool = pooling.MySQLConnectionPool(
+    pool_name="my_connection_pool",
+    pool_size=5,
+    pool_reset_session=True,
     host="localhost",
     user="root",
     password="12345678",
-    database="taipeidaytrip",
+    database="taipeidaytrip"
 )
-cursor=db.cursor()
-    
-cursor.execute ("CREATE TABLE attractions(id bigint NOT NULL AUTO_INCREMENT PRIMARY KEY, name varchar(255) NOT NULL,category varchar(255) NOT NULL, description varchar(3000) NOT NULL,address varchar(255) NOT NULL, transport varchar(3000) NOT NULL,mrt varchar(255),latitude float NOT NULL,longitude float NOT NULL,images JSON NOT NULL)")
+
+attractions_table = """CREATE TABLE attractions(
+                    id bigint NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+                    name varchar(255) NOT NULL,category varchar(255) NOT NULL, 
+                    description varchar(3000) NOT NULL,address varchar(255) NOT NULL, 
+                    transport varchar(3000) NOT NULL,mrt varchar(255),
+                    latitude float NOT NULL,
+                    longitude float NOT NULL,
+                    images JSON NOT NULL)"""
+connection_object = connection_pool.get_connection()
+cursor = connection_object.cursor()
+cursor.execute(attractions_table)
 
 
 with open("taipei-attractions.json", "r",encoding="utf-8") as file:
@@ -19,16 +33,24 @@ data = data["result"]["results"]
 for x in data:
     images=x["file"]
     images=images.replace("JPG","jpg").split("jpg")
+    # print(images)
     for i in images:
         #print(i)
         if i == "" or i[-1] != ".":
-  	        images.remove(i)
-       
+            images.remove(i) 
     images=[str(x)+"jpg" for x in images]# 這個就是整理好的
-
     img=json.dumps(images)
 
     sql = 'INSERT INTO attractions (name, category, description, address, transport, mrt, latitude, longitude,images) VALUES (%s, %s, %s, %s, %s, %s, %s, %s,%s)'
     val = (x["name"],x["CAT"],x["description"],x["address"],x["direction"],x["MRT"],x["latitude"],x["longitude"],img)
     cursor.execute(sql, val)
-    db.commit()
+    connection_object.commit()
+
+try:
+    connection_object = connection_pool.get_connection()
+    cursor = connection_object.cursor()
+except Error as e:
+    print("Error while connecting to MySQL using Connection pool ", e)
+finally:
+    cursor.close()
+    connection_object.close()
