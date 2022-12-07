4e4bb7abde3c28c9044b3e857d87356956816638
HuangYiChen <s1094343@g.ncyu.edu.tw>
2022-12-07 17:05:39 +0800

    week3 first version

--- static/js/attraction.js
+++ static/js/attraction.js
@@ -1,70 +1,100 @@
 let attraction = document.querySelector(".attraction")
-let text = document.querySelector(".text");
-// let attractionName = document.querySelector(".attractionName")
-// let attractionCat = document.querySelector("attractionCat")
+let colright = document.querySelector(".col-right");
+
 let url = location.href
 let id = url.split("attraction/")[1]
 url = "/api/attractions/" + id
+let slideIndex = 1;
 //create attraction
 function createAttraction(data){
-    console.log(data)
     let name = data.name;
     let mrt = data.mrt;
     let category = data.category;
-    let image = data.images[0];
+    let imagelist = data.images;
     let description = data.description;
     let address = data.address;
     let transport = data.transport;
 
-    // attractionName.textContent = name;
-    // attractionCat.textContent = `${category} at ${mrt}`; 
-    
-    let htmltopright = "";
-    // <div class="col-right">
-    //             <img class="attractionImg" src=${image} alt="" />
-    //     </div>
-    htmltopright +=` 
-        <div class="col-right">
-            <div class="slideshow-container">
+    // create appendimg and dot
+    for (i in imagelist){
+       appendimg(imagelist[i])
+    }
+    createarrowimg();
+    //name,category,mrt 文字
+    createtoprightHtml(name,category,mrt)
+    //文字段落
+    createbottomHtml(description,address,transport);
 
-                <div class="mySlides fade" style="display:block">
-                <img  class="attractionImg" src="https://www.travel.taipei/d_upload_ttn/sceneadmin/pic/11000848.jpg">
-                </div>
-            
-                <div class="mySlides fade" style=display:none>
-                <img  class="attractionImg" src="https://www.travel.taipei/d_upload_ttn/sceneadmin/pic/11000340.jpg">
-                </div>
-            
-                <div class="mySlides fade" style=display:none>
-                <img class="attractionImg" src="https://www.travel.taipei/d_upload_ttn/sceneadmin/image/A0/B0/C0/D7/E150/F719/71eb4b56-f771-43bc-856c-2fb265a5cc6e.jpg" >
-                </div>
-            
-                <a class="prev" onclick="plusSlides(-1)"><img src="/static/img/btn_leftArrow.png" alt=""/></a>
-                <a class="next" onclick="plusSlides(1)"><img src="/static/img/btn_rightArrow.png" alt=""/></a>
-            
-                <div  class="dots-container">
-                    <span class="dot" onclick="currentSlide(1)"></span> 
-                    <span class="dot" onclick="currentSlide(2)"></span> 
-                    <span class="dot" onclick="currentSlide(3)"></span> 
-                </div>
-            
-            </div>
-        </div>
-        <div class="col-left">
-            <div class="attractionName">${name}</div>
-            <div class="attractionCat">${category} at ${mrt}</div>
-            
-            <div class="form">
-                <div class="one">訂購導覽行程</div>
-                <div class="two">以此景點為中心的一日行程，帶您探索城市角落故事</div>
-                <span class="three">選擇日期:</span> <input  type="date">
-                <div class="four">選擇時間: <input type="radio"/>上半天<input type="radio"/>下半天</div>
-                <div class="five">導覽費用:請選擇費用</div>
-                <div class="six">開始預約行程</div>
-            </div>
-        </div>
-    `
-    attraction.innerHTML= htmltopright;
+    showSlides(slideIndex);
+
+};
+
+
+
+//fetch url return json data
+async function getattractionData(url){
+    const response = await fetch(url);
+    const json = await response.json();
+    let data = json.data; 
+    createAttraction(data);
+};
+getattractionData(url);
+
+
+let slideshowcontainer = document.querySelector(".slideshow-container");
+let dotcontainers = document.createElement("div");
+dotcontainers.classList = "dots-container"
+let dotcontainer = document.querySelector(".dots-container");
+//create appendimg and dot
+function appendimg(img){
+    let myslidesfade = document.createElement("div");
+    myslidesfade.classList = "mySlides fade";
+    // myslidesfade.setAttribute("style","display:none");
+    let slideimg = document.createElement("img")
+    slideimg.classList = "attractionImg"
+    slideimg.src = img
+    myslidesfade.appendChild(slideimg)
+    slideshowcontainer.appendChild(myslidesfade)
+
+    let dot = document.createElement("span");
+    dot.classList = "dot"
+    // dot.onclick = function(){currentSlide(i)};
+    dotcontainers.appendChild(dot)
+    slideshowcontainer.appendChild(dotcontainers)
+    
+}
+//arrow img
+function createarrowimg(){
+    let atag1 = document.createElement("a")
+    atag1.classList="prev"
+    atag1.onclick = function(){plusSlides(-1)}
+    let leftarrowimg = document.createElement("img");
+    leftarrowimg.setAttribute("src","/static/img/btn_leftArrow.png")
+    leftarrowimg.setAttribute("alt","")
+    atag1.appendChild(leftarrowimg)
+    let atag2 = document.createElement("a")
+    atag2.classList="next"
+    atag2.onclick = function(){plusSlides(1)}
+    let rightarrowimg = document.createElement("img");
+    rightarrowimg.setAttribute("src","/static/img/btn_rightArrow.png")
+    rightarrowimg.setAttribute("alt","")
+    atag2.appendChild(rightarrowimg)
+    slideshowcontainer.appendChild(atag1)
+    slideshowcontainer.appendChild(atag2)
+
+    let slideIndex = 1;
+    showSlides(slideIndex);
+}
+//name,category,mrt 文字
+function createtoprightHtml(name,category,mrt){   
+    let attractionName = document.querySelector(".attractionName")
+    let attractionCat = document.querySelector(".attractionCat")
+    attractionName.textContent = name;
+    attractionCat.textContent = category + "at" + mrt; 
+}
+//文字段落
+function createbottomHtml(description,address,transport){
+    let text = document.querySelector(".text");
     let htmlbottom = ""
     htmlbottom+=` 
         <div class="bottom">
@@ -80,41 +110,48 @@ function createAttraction(data){
         </div>
     `
     text.innerHTML= htmlbottom;
-};
+}
+
+//不同段不同價錢
+let cost = document.querySelector(".cost")
+let am = document.querySelector(".am")
+let pm = document.querySelector(".pm")
+window.onclick = function(event){
+    if(event.target == am){
+        cost.innerHTML=""
+        cost.innerHTML = "新台幣 2000 元"
+        pm.checked = false;
+
+    }
+    if(event.target == pm){
+        cost.innerHTML=""
+        cost.innerHTML = "新台幣 2500 元"
+        am.checked = false;
+    }
+}
 
 
-//fetch url return json data
-async function getattractionData(url){
-    const response = await fetch(url);
-    const json = await response.json();
-    let data = json.data; 
-    createAttraction(data);
-};
-getattractionData(url);
-
-let slideIndex = 1;
-showSlides(slideIndex);
 
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+showSlides(slideIndex += n);
 }
 
 function currentSlide(n) {
-  showSlides(slideIndex = n);
+showSlides(slideIndex = n);
 }
 
 function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
+let i;
+let slides = document.getElementsByClassName("mySlides");
+let dots = document.getElementsByClassName("dot");
+if (n > slides.length) {slideIndex = 1}    
+if (n < 1) {slideIndex = slides.length}
+for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";  
-  }
-  for (i = 0; i < dots.length; i++) {
+}
+for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
+}
+slides[slideIndex-1].style.display = "block";  
+dots[slideIndex-1].className += " active";
 }
